622fb91721659e79fd0a3935c33bcdf9d8ecd087
kkangh00n <kig2454@gmail.com>
2023-11-29 14:34:06 +0900

    feat: 순수 DI 컨테이너 문제점 (동시성 X)

---
+++ src/test/java/com/inflearn/springbasic/singleton/SingletonTest.java
@@ -0,0 +1,23 @@
+package com.inflearn.springbasic.singleton;
+
+import com.inflearn.springbasic.config.AppConfig;
+import com.inflearn.springbasic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+    }
+}
