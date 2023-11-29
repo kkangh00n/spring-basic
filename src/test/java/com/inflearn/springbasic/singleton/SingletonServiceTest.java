b28e3a48d3348b421b74d8b9bbc36af10141a7e2
kkangh00n <kig2454@gmail.com>
2023-11-29 17:23:13 +0900

    feat: 싱글톤 컨테이너 적용 테스트

--- src/test/java/com/inflearn/springbasic/singleton/SingletonServiceTest.java
+++ src/test/java/com/inflearn/springbasic/singleton/SingletonServiceTest.java
@@ -2,7 +2,11 @@ package com.inflearn.springbasic.singleton;
 
 import static org.assertj.core.api.Assertions.*;
 
+import com.inflearn.springbasic.config.AppConfig;
+import com.inflearn.springbasic.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonServiceTest {
 
@@ -12,5 +16,15 @@ public class SingletonServiceTest {
         SingletonService singletonService2 = SingletonService.getInstance();
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    void SpringContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService).isSameAs(memberService2);
     }
 }
